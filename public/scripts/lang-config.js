2c19b248a8c30b3d9b620b5c6d39eca8516018b1
Tausif Fardin <tausiffardin4@gmail.com>
2024-01-10 14:45:07 +0600

    multi-language page using google translator script

---
+++ public/scripts/lang-config.js
@@ -0,0 +1,10 @@
+window.__GOOGLE_TRANSLATION_CONFIG__ = {
+  languages: [
+    { title: "English", name: "en" },
+    { title: "Bengali", name: "bn" },
+    { title: "Deutsch", name: "de" },
+    { title: "Español", name: "es" },
+    { title: "Français", name: "fr" },
+  ],
+  defaultLanguage: "en",
+};
---
+++ src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Docs page</div>;
+};
+
+export default page;
